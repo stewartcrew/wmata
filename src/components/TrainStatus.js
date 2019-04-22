0bc9cf210bdd591837b1ddb0e1de0f95a9a7e7c4
Stewart Crew <jcrew@tuskdigital.com>
2019-04-22 16:26:26 -0400

    Add Bootstrap

--- src/components/TrainStatus.js
+++ src/components/TrainStatus.js
@@ -1,21 +1,21 @@
 import React from 'react';
 
-import './TrainStatus.css';
+import './TrainStatus.scss';
 
 class TrainStatus extends React.Component {
   render() {
     return (
-      <div>
-        <h2>{this.props.locationName}</h2>
+      <div className="row">
+        <h2 className="mt-4 col-12">{this.props.locationName}</h2>
         {this.props.trains.map((train, i) => (
-          <div key={`train-${i}`} className="results-group">
+          <div key={`train-${i}`} className="results-group my-4 col-4">
             <span className={`train-line line-${train.Line}`}></span>
             <div>Destination: {train.DestinationName}</div>
             <div>{train.Car}-car train</div>
             <div>{train.Min} mins away</div>
           </div>
         ))}
-        <small>Data provided my Washington Metropolitan Area Transit Authority (<a href="https://www.wmata.com/" target="_blank" rel="noreferrer noopener">WMATA.com</a>)</small>
+        <small className="col-12">Data provided my Washington Metropolitan Area Transit Authority (<a href="https://www.wmata.com/" target="_blank" rel="noreferrer noopener">WMATA.com</a>)</small>
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
+import './custom.scss';
+import './index.scss';
 import App from './App';
+
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
