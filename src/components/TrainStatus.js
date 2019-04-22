4a9b7b660c601883d91a0db72507320e3553d817
Stewart Crew <jcrew@tuskdigital.com>
2019-04-22 19:50:02 -0400

    Update UX components

--- src/components/TrainStatus.js
+++ src/components/TrainStatus.js
@@ -4,21 +4,38 @@ import './TrainStatus.scss';
 
 class TrainStatus extends React.Component {
   render() {
+
     return (
       <div className="row">
         <h2 className="mt-4 col-12">{this.props.locationName}</h2>
         {this.props.trains.map((train, i) => (
-          <div key={`train-${i}`} className="results-group p-3 col-12 col-md-4">
+          <div key={`train-${i}`} className="results-group p-3 col-lg-4 col-12">
             <div className="row">
-              <div className="col-8">
+              <div className="col-8 my-auto">
                 <span className={`train-line line-${train.Line}`}></span>
-                <small>{train.Car} cars</small>
+
+                {train.Car > 1 ?
+                  <small>{train.Car} cars</small>
+                  :
+                  <small>&nbsp;</small>
+                }
+
                 <h5>{train.DestinationName}</h5>
               </div>
-              <div className="col-4 wait-time">
+
+              <div className="col-4 wait-time my-auto">
                 <h1 className="mb-0">{train.Min}</h1>
-                <p>minutes</p>
+
+                {train.Min > 1 &&
+                  <p>minutes</p>
+                }
+
+                {train.Min === '1' &&
+                  <p>minute</p>
+                }
+
               </div>
+
             </div>
           </div>
         ))}
